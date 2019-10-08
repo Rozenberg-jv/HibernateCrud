4ff26a83a45b1ff1b9c359e536260a5b1e594c53
Rozenberg-jv <avangard.npaper@gmail.com>
2019-10-08 16:02:55 +0300

    refill libs

--- src/by/kolbun/andersen/hibernate_xml_example/HibernateUtil.java
+++ src/by/kolbun/andersen/hibernate_xml_example/HibernateUtil.java
@@ -3,33 +3,35 @@ package by.kolbun.andersen.hibernate_xml_example;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory;
+	private static final SessionFactory sessionFactory;
 
-    static {
-        try {
-            Configuration configuration = new Configuration();
-            configuration.configure("by/kolbun/andersen/hibernate_xml_example/resources/hibernate.cfg.xml");
+	static {
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure("by/kolbun/andersen/hibernate_xml_example/resources/hibernate.cfg.xml");
 
-            ServiceRegistry serviceRegistry = new ServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).buildServiceRegistry();
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+					.applySettings(configuration.getProperties()).build();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 
-        } catch (Throwable ex) {
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
+		} catch (Throwable ex) {
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
 
-    public static Session getSession() throws HibernateException {
-        return sessionFactory.openSession();
-    }
+	public static Session getSession() throws HibernateException {
 
-    private static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
+		return sessionFactory.openSession();
+	}
+
+	private static SessionFactory getSessionFactory() {
+
+		return sessionFactory;
+	}
 }
